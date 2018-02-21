ffe34a33491f9d79b5eae3fd0fb8565beac203ec
Khallil <khallil.doudou@epitech.eu>
2018-02-21 08:27:14 +0800

    work on gpu

--- ML Code/code/deep_learning/framework_practice/tensorflow/gpu_tf.py
+++ ML Code/code/deep_learning/framework_practice/tensorflow/gpu_tf.py
@@ -15,15 +15,15 @@ a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+#sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # Runs the op.
-print(sess.run(c))
+#print(sess.run(c))
 
 from tensorflow.python.client import device_lib
 
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+    return [x.name for x in local_device_protos]
 
 names = get_available_gpus()
 print names
